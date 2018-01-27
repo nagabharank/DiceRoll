884730519b70e28ccfa48cfa55604ada4eef5b0e
NagaBharan Kothrui <bharan@NagaBharans-MacBook-Pro.local>
2018-01-26 23:55:46 -0700

    first commit

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDice1: Int = 0
+    var randomDice2: Int = 0
+    
+    var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    @IBOutlet weak var diceImg1: UIImageView!
+    @IBOutlet weak var diceImg2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        diceImageChange()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+
+    @IBAction func rollBtnPressed(_ sender: UIButton) {
+        diceImageChange()
     }
-
-
+    
+    func diceImageChange() {
+        randomDice1 = Int(arc4random_uniform(6))
+        randomDice2 = Int(arc4random_uniform(6))
+        
+        diceImg1.image = UIImage(named: diceArray[randomDice1])
+        diceImg2.image = UIImage(named: diceArray[randomDice2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        diceImageChange()
+    }
+    
 }
 
